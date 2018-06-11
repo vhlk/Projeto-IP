7331e1a5e644bd325bd90c18f288eee098cd62dd
eidsonsa <eidsonsa@gmail.com>
2018-06-11 09:57:41 -0300

    Add files via upload

---
+++ Projeto-IP-master/proj/banco/Pratos.java
@@ -0,0 +1,23 @@
+public class Pratos{
+	private String nome;
+	private double preco;
+	
+	public Pratos(String nomeP, double precoP) {
+		this.nome = nomeP;
+		this.preco = precoP;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void atualizarPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+}
