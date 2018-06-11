2f87298ebf10624aae18de67200089e505e2af66
Maria Giovana Accioly Santos <mgas@WINDOWS.CIN.UFPE.BR>
2018-06-11 09:40:11 -0300

    Classes iniciais

---
+++ projeto/ProjetoIP/src/classes/Disciplina.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Disciplina {
+	private String disciplina;
+	private double preco;
+	
+	Disciplina() {
+		this.disciplina = "";
+		this.preco = 0;
+	}
+}
---
+++ projeto/ProjetoIP/src/classes/Material.java
@@ -0,0 +1,27 @@
+package classes;
+
+public class Material {
+	private String titulo;
+	private String tipo;
+	
+	public Material() {
+		this.titulo = null;
+		this.tipo = null;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
